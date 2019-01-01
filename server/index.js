b9896cf50a95499cded2d72e18165c38f91de50f
kazuma takiuchi <basstrombone413@gmail.com>
2019-01-01 16:59:29 +0900

    fix parameter serach

--- server/index.js
+++ server/index.js
@@ -4,12 +4,71 @@ const { buildSchema } = require("graphql");
 
 const schema = buildSchema(`
     type Query{
-        message:String
+        course(id: Int!): Course
+        courses(topic: String): [Course]
+    },
+    type Course{
+        id: Int
+        title: String
+        author: String
+        description: String
+        topic: String
+        url: String
     }
 `);
 
+const coursesData = [
+  {
+    id: 1,
+    title: "The Complete Node.js Developer Course",
+    author: "Andrew Mead, Rob Percival",
+    description:
+      "Learn Node.js by building real-world applications with Node, Express, MongoDB, Mocha, and more!",
+    topic: "Node.js",
+    url: "https://codingthesmartway.com/courses/nodejs/"
+  },
+  {
+    id: 2,
+    title: "Node.js, Express & MongoDB Dev to Deployment",
+    author: "Brad Traversy",
+    description:
+      "Learn by example building & deploying real-world Node.js applications from absolute scratch",
+    topic: "Node.js",
+    url: "https://codingthesmartway.com/courses/nodejs-express-mongodb/"
+  },
+  {
+    id: 3,
+    title: "JavaScript: Understanding The Weird Parts",
+    author: "Anthony Alicea",
+    description:
+      "An advanced JavaScript course for everyone! Scope, closures, prototypes, this, build your own framework, and more.",
+    topic: "JavaScript",
+    url: "https://codingthesmartway.com/courses/understand-javascript/"
+  }
+];
+
+var getCourse = args => {
+  const id = args.id;
+
+  return coursesData.filter(course => {
+    return course.id === id;
+  })[0];
+};
+
+var getCourses = args => {
+  if (args.topic) {
+    const topic = args.topic;
+    return coursesData.filter(course => {
+      return course.topic === topic;
+    });
+  } else {
+    return coursesData;
+  }
+};
+
 const root = {
-  message: () => "Hello Graphql!"
+  course: getCourse,
+  courses: getCourses
 };
 
 const app = express();
